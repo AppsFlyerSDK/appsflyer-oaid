bb593ef285f487ff038832b76247ddd215a19411
Dmytro Vasylkov <dmytro.vasylkov@appsflyer.com>
2021-02-01 17:12:23 +0200

    Huawei timeout

--- app/src/main/kotlin/com/appsflyer/oaid/app/MainActivity.kt
+++ app/src/main/kotlin/com/appsflyer/oaid/app/MainActivity.kt
@@ -2,7 +2,7 @@ package com.appsflyer.oaid.app
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import com.appsflyer.oaid.OaidClient
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.concurrent.TimeUnit
--- oaid/src/main/java/com/appsflyer/oaid/OaidMsaClient.java
+++ oaid/src/main/java/com/appsflyer/oaid/OaidMsaClient.java
@@ -1,7 +1,7 @@
 package com.appsflyer.oaid;
 
 import android.content.Context;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.text.TextUtils;
 
 import com.bun.miitmdid.core.ErrorCode;
